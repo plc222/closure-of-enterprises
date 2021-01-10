42662fdec555b8001847f575991649370f1b5535
plc222 <yurij.altay220@yandex.ru>
2021-01-10 17:42:18 +0700

    Сверстал блок SLIDER

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 $(function () {
- 
+   $(".slider__inner").slick(
+      {
+         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+         infinite:false,
+         
+      }
+    );
    
 
-   
+
 });
    
 
